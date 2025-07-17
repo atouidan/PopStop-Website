afbb2b0059cf44ad92f4e3073c63b2bfddc1d070
atouidan <atouidana@gmail.com>
2025-07-17 14:49:49 -0400

    Fix redirecting page

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/style.css");
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("_redirects");
 
   return {
     dir: {
